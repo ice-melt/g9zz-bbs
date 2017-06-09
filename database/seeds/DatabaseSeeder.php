50428e00cee917ff7228cc4b851b9e858e8a8479
ylsc <2067930913@qq.com>
2017-06-09 18:38:49 +0800

    add database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
          $this->call(NodeTableSeeder::class);
-
     }
 }
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -12,10 +12,14 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $a = factory(User::class,10)->create();
+        $a = factory(User::class,1)->create();
+        $b = factory(\App\Models\Roles::class,1)->create();
+
         foreach ($a as $value) {
             $value->hid = \Vinkla\Hashids\Facades\Hashids::connection('user')->encode($value->id);
             $value->save();
+            $value->role()->sync([$b[0]->id]);
         }
+
     }
 }
