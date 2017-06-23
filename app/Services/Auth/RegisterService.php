94d71d7dc8daa94cb842cb7b6ffbe7bb0516533b
ylsc <2067930913@qq.com>
2017-06-23 17:12:42 +0800

    add avatar

--- app/Services/Auth/RegisterService.php
+++ app/Services/Auth/RegisterService.php
@@ -40,6 +40,7 @@ class RegisterService extends BaseService
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'password' => bcrypt($request->get('password')),
+            'avatar' => 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1497014683027&di=3cfb152ddbcdb74cc2840e92022e6888&imgtype=0&src=http%3A%2F%2Fwww.ucicq.com%2Fuploads%2Fallimg%2F170307%2F2234302133_0.jpg',
         ];
         return  $this->userService->loginCreate($create,$other);
     }
