575e45908838ac9a8c865f579a9b3fe2697bcbd2
ylsc <2067930913@qq.com>
2017-11-15 22:34:14 +0800

    :fire: wechat

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -17,51 +17,23 @@ class WechatController extends Controller
 {
 
     protected $app;
-    protected $wechat;
 
 
     public function __construct()
     {
         $this->app = new Application(config('wechat'));
-        $this->wechat = app('wechat');
     }
 
     public function serve()
     {
-//        $this->log('22222222222');
+        $this->log('22222222222');
 
         $this->app->server->setMessageHandler(function ($message) {
-            switch ($message->MsgType) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
-            // ...
+            return "叶落山城秋";
+
         });
 
-//        $this->log('233455555');
+        $this->log('233455555');
 
         return $this->app->server->serve();
 
