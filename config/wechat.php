67d3db37a8309d89829b9b023a793633991288e5
ylsc <2067930913@qq.com>
2017-11-15 21:42:57 +0800

    :fire: wechat

--- config/wechat.php
+++ config/wechat.php
@@ -100,9 +100,9 @@ return [
      */
     'enable_mock' => env('WECHAT_ENABLE_MOCK', false),
     'mock_user' => [
-        'openid' => 'odh7zsgI75iT8FRh0fGlSojc9PWM',
+        'openid' => 'o7KWGwOqvSvgwlrcyyvy_2f2WfcU',
         // 以下字段为 scope 为 snsapi_userinfo 时需要
-        'nickname' => 'overtrue',
+        'nickname' => 'yeluo',
         'sex' => '1',
         'province' => '北京',
         'city' => '北京',
