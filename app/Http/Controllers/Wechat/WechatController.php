dc4002a02a80a815ba99038710c81a95543d16cf
ylsc <2067930913@qq.com>
2017-11-17 18:40:19 +0800

    :fire: wechat

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -10,6 +10,7 @@ namespace App\Http\Controllers\Wechat;
 
 use App\Http\Controllers\Controller;
 use EasyWeChat\Foundation\Application;
+use EasyWeChat\Message\Text;
 
 class WechatController extends Controller
 {
@@ -23,11 +24,77 @@ class WechatController extends Controller
     {
         $server = $this->app->server;
         $server->setMessageHandler(function($message){
-            return "叶落山城秋";
+            switch ($message->MsgType) {
+                case 'event':
+                    return $this->event($message);
+                    break;
+                case 'text':
+                    return $this->text($message);
+                    break;
+                case 'image':
+                    return '暂不处理图片消息';
+                    break;
+                case 'voice':
+                    return '暂不处理语音消息';
+                    break;
+                case 'video':
+                    return '暂不处理视频消息';
+                    break;
+                case 'location':
+                    return '暂不处理坐标消息';
+                    break;
+                case 'link':
+                    return '暂不处理链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '暂不处理其它消息';
+                    break;
+            }
         });
 
         $this->log('return response.');
 
         return $server->serve();
+    }
+
+    /**
+     * @param $message
+     * @return string
+     */
+    public function event($message)
+    {
+        switch ($message->Event) {
+            case 'subscribe':
+                return "欢迎关注";
+                break;
+            case "unsubscribe":
+                return "卧槽";
+                break;
+            case "CLICK":
+                return "点击个啥";
+                break;
+            case 'VIEW':
+                return "卧槽";
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * @param $message
+     * @return Text
+     */
+    public function text($message)
+    {
+        $content = $message->Content;
+
+        if($content == '叶落') {
+            $text = new Text(['content' => '叶落山城秋1']);
+        } else {
+            $text = new Text(['content' => '叶落山城秋2']);
+        }
+        return $text;
     }
 }
